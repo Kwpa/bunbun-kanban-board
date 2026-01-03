ba98f2cb884a1b73f5ccfd608d917cccaff1aee1
Kwpa <kai.mcgilligan.oliver@gmail.com>
2026-01-03 00:24:17 -0400

    [Kai] Green + touchscreen fix attempt

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -127,7 +127,7 @@ export default function Card({ card }) {
   return (
     <div
       className={
-        `Card flex flex-col hover:cursor-pointer bg-gray-800 hover:text-violet-400 h-auto rounded-md p-2 mb-4 border-t border-t-blue-200 border-opacity-10 shadow-md transition-transform ` +
+        `draggable-card Card flex flex-col hover:cursor-pointer bg-gray-800 hover:text-violet-400 h-auto rounded-md p-2 mb-4 border-t border-t-blue-200 border-opacity-10 shadow-md transition-transform ` +
         card.color
       }
       // style={{
